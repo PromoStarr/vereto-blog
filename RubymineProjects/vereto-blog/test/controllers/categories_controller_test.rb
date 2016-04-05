5b61411cf64c4611c11a68c32d5e85cc285aff0c
Promo <faceeatingtumor@gmail.com>
2016-04-05 19:52:56 +0100

    Admin and Testing Created

---
+++ RubymineProjects/vereto-blog/test/controllers/categories_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class CategoriesControllerTest < ActionController::TestCase
+
+  def setup
+    @category = Category.create(name: "Dick Fortress")
+    @admin = User.create(username: "John", email: "John@example.com", password: "password", admin: true)
+  end
+
+  test "should get categories index" do
+
+    get :index
+    assert_response :success
+  end
+
+  test "admin should get new" do
+    session[:user_id] = @admin.id
+    get :new
+    assert_response :success
+  end
+
+  test "should get show" do
+    get(:show, {'id' => @category.id })
+    assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "Dicks Fortress" }
+    end
+    assert_redirected_to categories_path
+  end
+
+end
